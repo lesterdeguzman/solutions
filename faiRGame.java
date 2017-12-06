ac0cd82e1acd02918841f25b5d136c3bf3f11856
snshinejoy <2162393@slu.edu.ph>
2017-12-06 20:26:11 +0800

    last na last

---
+++ faiRGame.java
@@ -0,0 +1,162 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.InputStream;
+
+public class faiRGame {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        fairGame solver = new fairGame();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class fairGame {
+        public void solve(int testNumber, InputReader scan, OutputWriter out) {
+            int array[] = new int[105];
+            HashMap<Integer, Integer> map = new HashMap<>();
+            HashSet<Integer> set = new HashSet<>();
+            ArrayList<Integer> list = new ArrayList<>();
+            int n = scan.nextInt();
+            for (int i = 0; i < n; i++) {
+                int a = scan.nextInt();
+                if (map.containsKey(a)) {
+                    map.put(a, map.get(a) + 1);
+                } else {
+                    map.put(a, 1);
+                }
+                array[a]++;
+            }
+            if (map.size() != 2) {
+                out.println("NO");
+                return;
+            } else {
+                for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                    list.add(entry.getKey());
+                }
+                if (map.get(list.get(0)) != map.get(list.get(1))) {
+                    out.println("NO");
+                } else {
+                    out.println("YES");
+                    out.println(list.get(0), list.get(1));
+                }
+            }
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void println(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
